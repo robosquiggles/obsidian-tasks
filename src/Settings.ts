13ff1d3da869ea545fe17acfce4b2a5b858fd922
Martin Schenck <martin.schenck@futurice.com>
2021-11-02 18:23:04 +0100

    Support custom date formats
    Incorporating and cleaning up the changes proposed in #296.

--- src/Settings.ts
+++ src/Settings.ts
@@ -1,15 +1,18 @@
+export const dateFormats = ['YYYY-MM-DD', 'YYYYMMDD'];
+export type DateFormat = typeof dateFormats[number]; // All formats in the array of dateFormats.
+
 export interface Settings {
     globalFilter: string;
     removeGlobalFilter: boolean;
-    dateFormat: string;
-    dateLink: boolean;
+    dateFormat: DateFormat;
+    linkDates: boolean;
 }
 
 const defaultSettings: Settings = {
     globalFilter: '',
     removeGlobalFilter: false,
     dateFormat: 'YYYY-MM-DD',
-    dateLink: false,
+    linkDates: false,
 };
 
 let settings: Settings = { ...defaultSettings };
